f2d44fdc416b87d6f7497e170032ca2652147877
Nathan Morrison <morrna@gmail.com>
2015-08-14 13:11:48 -0600

    Initial commit! Very little has changed since downloading the example code from the Google App Engine tutorial page.

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello! This is a personal project owned by Nathan Morrison.' \
+				+ '<br>Content is likely to change drastically without warning.' \
+				+ '<br><br>What are you doing here, anyway?'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
