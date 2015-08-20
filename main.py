78f58e582fba82f626c95c4043755eb0970e638e
Nathan Morrison <morrna@gmail.com>
2015-08-20 13:02:19 -0600

    created new '/wall/' route to try forms
    After reading up on forms in the Flask quickstart, I decided to start
    implementing a simple wall for posting a limited number of messages so I could
    get a hang of request objects and so on. I quickly became overwhelmed by trying
    to figure out how to store the data, so for right now I settled on just leaving
    the route with an "under construction" page.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
+### 
 @app.route('/')
 def hello():
     "Displays front page."
@@ -13,13 +13,48 @@ def hello():
 
 @app.route('/about/')
 def about_message():
-	with open('content/about_msg.htm','r') as f:
-		about_msg_str = f.read()
-	
-	return render_template("simple_page.html", pagetitle="About"
-								, content=about_msg_str)
+    with open('content/about_msg.htm','r') as f:
+        about_msg_str = f.read()
+    
+    return render_template("simple_page.html", pagetitle="About"
+                                , content=about_msg_str)
+
+@app.route('/wall/', methods=['POST', 'GET'])
+def wall_page():
+    ## Temporarily deter interlopers
+    excuse_msg = "Sorry, but I'm not done figuring this part out yet." \
+            + " Come back later!"
+    return render_template("simple_page.html"
+                            , pagetitle = "Under Construction"
+                            , content = excuse_msg
+                            )
+
+    if request.method == 'POST':
+        my_wall.add_message(request.form["new_message"])
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+### Tools etc.
+class Wall:
+    """
+    An object for storing and loading a limited list of messages.
+    This will probably need to be updated to work off of the Datastore.
+    For now, the messages are not persistent.
+    """
+
+    def __init__(self):
+        self.max_messages = 12
+        self.messages = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+        if len(self.messages) > self.max_messages:
+            self.messages.pop(0)
+
+
+my_wall = Wall()
+
